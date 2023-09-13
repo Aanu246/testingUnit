2959ac442fae36235db2686bbed7828f79d7fec7
Aanu246 <95230551+Aanu246@users.noreply.github.com>
2023-09-13 20:56:28 +0100

    Add files via upload

---
+++ MvcTestingExample/src/main/java/com/luv2code/component/MvcTestingExampleApplication.java
@@ -0,0 +1,23 @@
+package com.luv2code.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import com.luv2code.component.models.CollegeStudent;
+
+@SpringBootApplication
+public class MvcTestingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcTestingExampleApplication.class, args);
+	}
+	
+	@Bean(name = "collegeStudent")
+	@Scope(value = "prototype")
+	CollegeStudent getCollegeStudent() {
+		return new CollegeStudent();
+	}
+
+}
